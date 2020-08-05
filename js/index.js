c8c0fad953c87cc2ffc6d836f8baf3f0ab0889fd
Cevno <62213254+cevno-cn@users.noreply.github.com>
2020-08-05 19:29:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,6 @@
 $(function(){
+	$("body,html").scrollTop(0);
 	var h = $("body").height();
-	$("body,html").scrollTop(2*h);
-	$("body,html").stop().animate({scrollTop:0},2000);
+	$("body,html").stop().animate({scrollTop:2*h},2000);
 	$("body").css("overflow","hidden");
-	// $("header").click(function(){
-	// 	var h = $("body").height();
-	// 	$("body,html").stop().animate({scrollTop:(2*h)},5000);
-	// });
-	// $("section").click(function(){
-	// 	$("body,html").stop().animate({scrollTop:0},5000);
-	// })
-	
 })
